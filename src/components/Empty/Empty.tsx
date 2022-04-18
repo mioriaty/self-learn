2e43c5fbed8b4ee6d6a6dade17ffa6d802070c91
gnoudami <mioriaty@gmail.com>
2022-04-18 16:48:03 +0700

    them dam la keo

--- src/components/Empty/Empty.tsx
+++ src/components/Empty/Empty.tsx
@@ -4,18 +4,18 @@ import { FC } from 'react';
 import { memo } from 'react';
 
 export interface EmptyProps {
-  emoijSize?: number;
+  emojiSize?: number;
   textSize?: number;
   text?: string;
 }
 
-const errEmoij = ['(^_^)b', "(='X'=)", '(o^^)o', '(·.·)', '(o_o)/', '(>_<)', '(^Д^)/', '(^-^*)', '(≥o≤)'];
+const errEmoji = ['(^_^)b', "(='X'=)", '(o^^)o', '(·.·)', '(o_o)/', '(>_<)', '(^Д^)/', '(^-^*)', '(≥o≤)'];
 
-const Empty: FC<EmptyProps> = ({ emoijSize = 50, textSize = 14, text = 'No data' }) => {
+const Empty: FC<EmptyProps> = ({ emojiSize = 50, textSize = 14, text = 'No data' }) => {
   return (
     <View css={{ textAlign: 'center' }}>
-      <Text color="gray4" size={emoijSize} fontFamily="secondary" css={{ fontWeight: 600 }}>
-        {errEmoij[_.random(0, errEmoij.length - 1)]}
+      <Text color="gray4" size={emojiSize} fontFamily="secondary" css={{ fontWeight: 600 }}>
+        {errEmoji[_.random(0, errEmoji.length - 1)]}
       </Text>
       <Text color="gray8" size={textSize}>
         {text}
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
